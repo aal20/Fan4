077ddf820f0c0dfdae8fb5b19f100b217f4cb718
Anthony Lim <limxx213@gmail.com>
2016-09-27 21:22:45 -0500

    Initial solution setup/scaffolding

---
+++ Fan4/Fan4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Fan4.MvcApplication" Language="C#" %>
---
+++ Fan4/Fan4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fan4.Startup))]
+namespace Fan4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
